c12be74cbe7969b3f2793dae8cdbfcc495327c2d
Maurice Kleine <hey@mauricekleine.com>
2022-03-31 10:18:40 +0200

    Update install.sh
    Update brew install script

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # install brew
 brew_bin=$(which brew) 2>&1 > /dev/null
 if [[ $? != 0 ]]; then
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
   brew update
 fi
